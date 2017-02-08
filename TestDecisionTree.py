e7ef62e911175f2467c64a2b918b11722e9e0463
roland <wuzifan0817@gmail.com>
2017-02-08 12:20:50 +0000

    Merge branch 'master' of https://github.com/w460461339/MachineLearningCW2
    # Conflicts:
    #	LearningDecisionTree.py
    #	TestDecisionTree.py

--- TestDecisionTree.py
+++ TestDecisionTree.py
@@ -4,9 +4,11 @@ import math as mt
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+
 # from graphviz import Digraph
 
 __Author__ = 'Tree_Diagram'
+
 
 # 将选中的表情设置为1，其他为0,对应binary_targets
 def choose_emotion(facial_expression, emotion):
@@ -18,19 +20,21 @@ def choose_emotion(facial_expression, emotion):
             choosen_emotion.append(0)
     return choosen_emotion
 
+
 # 将属性编号为0到44，对应attributes
 def generate_attributes(num_attributes):
     return range(0, num_attributes)
 
+
 def examples_havesamevalue(binary_targets):
-    flag=True
-    if len(binary_targets) !=0:
-        target=binary_targets[0]
+    flag = True
+    if len(binary_targets) != 0:
+        target = binary_targets[0]
 
     # 遍历所有的example
-    for j in range(0,len(binary_targets)):
-        if target!=binary_targets[j]:
-            flag=False
+    for j in range(0, len(binary_targets)):
+        if target != binary_targets[j]:
+            flag = False
             break
     if flag:
         return target
@@ -103,21 +107,25 @@ def majority_value(binary_targets):
     else:
         return 0
 
-def generate_sub(examples,binary_targets,best_attribute,attribute_state):
-    myexamples=[]
-    mybinary_targets=[]
+
+def generate_sub(examples, binary_targets, best_attribute, attribute_state):
+    myexamples = []
+    mybinary_targets = []
     for ind, val in enumerate(examples):
-        if examples[ind][best_attribute]==attribute_state:
+        if examples[ind][best_attribute] == attribute_state:
             myexamples.append(examples[ind])
             mybinary_targets.append(binary_targets[ind])
-    return myexamples,mybinary_targets
+    return myexamples, mybinary_targets
+
 
 # 主要被调用函数
-TREE_NODES=[]
+TREE_NODES = []
+
+
 def DECISION_TREE_LEARNING(examples, attributes, binary_targets):
-    target_value=examples_havesamevalue(binary_targets)
-    if target_value!=-1:
-        if target_value==1:
+    target_value = examples_havesamevalue(binary_targets)
+    if target_value != -1:
+        if target_value == 1:
             node = [time.time(), 'YES', []]
             TREE_NODES.append(node)
             return node
@@ -125,9 +133,9 @@ def DECISION_TREE_LEARNING(examples, attributes, binary_targets):
             node = [time.time(), 'NO', []]
             TREE_NODES.append(node)
             return node
-    elif len(attributes)==0:
-        ma_value=majority_value(binary_targets)
-        if ma_value==1:
+    elif len(attributes) == 0:
+        ma_value = majority_value(binary_targets)
+        if ma_value == 1:
             node = [time.time(), 'YES', []]
             TREE_NODES.append(node)
             return node
@@ -136,14 +144,14 @@ def DECISION_TREE_LEARNING(examples, attributes, binary_targets):
             TREE_NODES.append(node)
             return node
     else:
-        best_attribute=choose_best_attribute(examples,attributes,binary_targets)
-        tree=[time.time(),str(attributes[best_attribute]),[]]
-        for attribute_state in [0,1]:
-            newexamples,newbinary_targets=generate_sub(examples,binary_targets,best_attribute,attribute_state)
-            if len(newexamples)==0:
+        best_attribute = choose_best_attribute(examples, attributes, binary_targets)
+        tree = [time.time(), str(attributes[best_attribute]), []]
+        for attribute_state in [0, 1]:
+            newexamples, newbinary_targets = generate_sub(examples, binary_targets, best_attribute, attribute_state)
+            if len(newexamples) == 0:
                 ma_value2 = majority_value(binary_targets)
                 if ma_value2 == 1:
-                    node=[time.time(), 'YES', []]
+                    node = [time.time(), 'YES', []]
                     TREE_NODES.append(node)
                     return node
                 else:
@@ -153,17 +161,18 @@ def DECISION_TREE_LEARNING(examples, attributes, binary_targets):
             else:
                 newattributes = attributes[:best_attribute] + attributes[best_attribute + 1:]
                 newexamples = map(lambda x: x[:best_attribute] + x[best_attribute + 1:], newexamples)
-                nextTreeNode=DECISION_TREE_LEARNING(newexamples,newattributes,newbinary_targets)
+                nextTreeNode = DECISION_TREE_LEARNING(newexamples, newattributes, newbinary_targets)
                 tree[2].append(nextTreeNode[0])
         TREE_NODES.append(tree)
     return tree
+
 
 def DrawDecisionTree(label, tree, dot):
     for node in tree:
         if node[0] == label:
             item = node
         break
-    [label, name, leaves]= item
+    [label, name, leaves] = item
     dot.node(label, name)
     if len(leaves) == 0:
         pass
@@ -174,66 +183,100 @@ def DrawDecisionTree(label, tree, dot):
         dot.edges(label, leaves[1], label='1')
     return dot
 
+
 def topythonlist(data):
-    mylist=[]
+    mylist = []
     for d in data:
         for dd in d:
             mylist.append(dd)
     return mylist
 
+
 def topythonnestedlist(data):
-    mynestedlist=[]
+    mynestedlist = []
     for da in data:
-        mylist=[]
+        mylist = []
         for dda in da:
             mylist.append(dda)
         mynestedlist.append(mylist)
     return mynestedlist
 
-# 预测函数
-def predictions(TreeList,testData):
-    labbel=[]
-    myfalg=False
+
+# 预测函数,随机
+def predictions(TreeList, testData):
+    labbel = []
+    myfalg = False
     for exam in testData:
-        for ind,tree in enumerate(TreeList):
-            root=tree[-1]
-            flag=find_labbel(exam,tree,root)
-            if flag==1:
-                myfalg=True
-                labbel.append(ind+1)
+        for ind, tree in enumerate(TreeList):
+            root = tree[-1]
+            flag,deepth = find_labbel(exam, tree, root)
+            if flag == 1:
+                myfalg = True
+                labbel.append(ind + 1)
                 break
-        if myfalg== False:
+        if myfalg == False:
             labbel.append(-1)
-        myfalg=False
+        myfalg = False
     return labbel;
 
-def find_labbel(exam,tree,root):
-    if root[1]=='YES':
-        return 1
-    elif root[1]=='NO':
-        return 0
-    else:
-        attribute_num=int(root[1])
-        real_au=exam[attribute_num]
-        if real_au==0:
-            next_node_index=0;
-            for ind,node in enumerate(tree):
-                if node[0]==root[2][0]:
-                    next_node_index=ind
-                    break
-            return find_labbel(exam,tree,tree[next_node_index])
+# 预测函数,带深度
+def predictions_deepth(TreeList, testData):
+    labbel = []
+    myfalg = False
+    for exam in testData:
+        maybeRigth=[]
+        for ind, tree in enumerate(TreeList):
+            root = tree[-1]
+            flag,deepth = find_labbel(exam, tree, root)
+            if flag == 1:
+                myfalg = True
+                maybeRigth.append([ind+1,deepth])
+
+        if myfalg == False:
+            labbel.append(-1)
         else:
-            next_node_index=0;
-            for ind,node in enumerate(tree):
-                if node[0]==root[2][1]:
-                    next_node_index=ind
+            final_result=0
+            final_deep=0
+            for vals in maybeRigth:
+                print "every deep is "+str(vals[1])
+                if vals[1]>final_deep:
+                    final_deep=vals[1]
+                    final_result=vals[0]
+            print "final_deep is "+str(final_deep)
+            labbel.append(final_result)
+        myfalg = False
+    return labbel;
+
+def find_labbel(exam, tree, root):
+    if root[1] == 'YES':
+        return (1,1)
+    elif root[1] == 'NO':
+        return (0,1)
+    else:
+        attribute_num = int(root[1])
+        real_au = exam[attribute_num]
+        if real_au == 0:
+            next_node_index = 0;
+            for ind, node in enumerate(tree):
+                if node[0] == root[2][0]:
+                    next_node_index = ind
                     break
-            return find_labbel(exam,tree,tree[next_node_index])
+            flag_label,deepth=find_labbel(exam, tree, tree[next_node_index])
+            return (flag_label,deepth+1)
+        else:
+            next_node_index = 0;
+            for ind, node in enumerate(tree):
+                if node[0] == root[2][1]:
+                    next_node_index = ind
+                    break
+            flag_label, deepth = find_labbel(exam, tree, tree[next_node_index])
+            return (flag_label, deepth + 1)
+
 
 if __name__ == "__main__":
     # 导入数据
     matfn = u'cleandata_students.mat'
-    #matfn = u'noisydata_students.mat'
+    # matfn = u'noisydata_students.mat'
 
     data = sio.loadmat(matfn)
 
@@ -246,14 +289,14 @@ if __name__ == "__main__":
     # for attribute
     attributes = generate_attributes(45)
 
-    for inx in range(0,10):
+    for inx in range(0, 10):
         test_examples = []
         train_examples = []
         test_facial_expression = []
         train_facial_expression = []
         for ind, val in enumerate(examples):
             # 选取10%作为test
-            if ind % 10 == inx: #当选取的test数据越少，其正确越越高
+            if ind % 10 == inx:  # 当选取的test数据越少，其正确越越高
                 test_examples.append(examples[ind])
                 test_facial_expression.append(facial_expression[ind])
             else:
@@ -270,6 +313,7 @@ if __name__ == "__main__":
             tree_list.append(TREE_NODES)
             TREE_NODES = []
 
+       # test_label = predictions_deepth(tree_list, test_examples)
         test_label = predictions(tree_list, test_examples)
 
         confusion_matrix = np.array([0] * 36).reshape(6, 6)
@@ -293,7 +337,6 @@ if __name__ == "__main__":
                                float(average_precision_rate[goal] + average_recall[goal]))
             correct_times += confusion_matrix[goal, goal]
 
-
         average_classification_rate = float(correct_times) / float(confusion_matrix.sum())
 
         print confusion_matrix
@@ -301,9 +344,3 @@ if __name__ == "__main__":
         print average_precision_rate
         print f1_measures
         print average_classification_rate
-
-
-
-
-
-
